cb29b5b1721dd7d42b1ebb0c4cb2140385158982
Diego de Souza <diegoguitaibanez@gmail.com>
2019-07-08 17:55:10 -0300

    primeiro commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+ // { path: '', redirectTo: 'home', pathMatch: 'full' },
+ { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+ { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+ // { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
+ // { path: 'tab2', loadChildren: './tab2/tab2.module#Tab2PageModule' },
+ // { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
