51bd407d02bdbfdd9052dfad97a827d14ab5dd0d
Diego <diegoguitaibanez@gmail.com>
2019-07-27 14:26:20 -0300

    feito page de novos usuarios

--- src/app/novo-usuario/novo-usuario.page.ts
+++ src/app/novo-usuario/novo-usuario.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoUsuarioPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService) { }
 
   ngOnInit() {
+  }
+  public logout() {
+    // tslint:disable-next-line: no-unused-expression
+    this.authService.logout();
   }
 
 }
